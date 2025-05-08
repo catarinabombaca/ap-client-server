bfab09c3552c72185700647c8644d3108228419d
catarinabombaca <catarina.bombaca@sedex.com>
2025-05-08 21:51:14 +0100

    bootstrap

---
+++ build.gradle.kts
@@ -0,0 +1,65 @@
+import org.gradle.api.JavaVersion.VERSION_21
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_21
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
+plugins {
+    kotlin("jvm") version "2.1.20"
+    application
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+        gradlePluginPortal()
+    }
+
+    dependencies {
+    }
+}
+
+kotlin {
+    jvmToolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+application {
+    mainClass = "com.example.ClientServerKt"
+}
+
+repositories {
+    mavenCentral()
+}
+
+tasks {
+    withType<KotlinJvmCompile>().configureEach {
+        compilerOptions {
+            allWarningsAsErrors = false
+            jvmTarget.set(JVM_21)
+            freeCompilerArgs.add("-Xjvm-default=all")
+        }
+    }
+
+    withType<Test> {
+        useJUnitPlatform()
+    }
+
+    java {
+        sourceCompatibility = VERSION_21
+        targetCompatibility = VERSION_21
+    }
+}
+
+dependencies {
+    implementation(platform("org.http4k:http4k-bom:6.8.1.0"))
+    implementation("org.http4k:http4k-client-okhttp")
+    implementation("org.http4k:http4k-core")
+    implementation("org.http4k:http4k-format-jackson")
+    implementation("org.http4k:http4k-server-undertow")
+    testImplementation("org.http4k:http4k-testing-approval")
+    testImplementation("org.http4k:http4k-testing-hamkrest")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.12.0")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.12.0")
+    testImplementation("org.junit.platform:junit-platform-launcher:1.12.2")
+}
+
---
+++ src/test/kotlin/com/clientserver/ClientServerTest.kt
@@ -0,0 +1,18 @@
+package com.clientserver
+
+import com.example.app
+import org.http4k.core.Method.GET
+import org.http4k.core.Request
+import org.http4k.core.Response
+import org.http4k.core.Status.Companion.OK
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class ClientServerTest {
+
+    @Test
+    fun `Ping test`() {
+        assertEquals(Response(OK).body("pong"), app(Request(GET, "/ping")))
+    }
+
+}
